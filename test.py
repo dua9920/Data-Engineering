a5fc1e0eab177f34ee2e0af5c11d6c17478f1b57
yeomss <dua9920@kakao.com>
2021-04-26 01:59:46 +0900

    create: 데이터 전처리 과제

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 import pandas as pd
-import da
+
+# df = pd.DataFrame({'시간': {'a':2.0, 'b':3.0, 'c':1.2}})
+df = pd.DataFrame({'시간': {'a': '2018-03 14:00', 'b': '2018-03 12:00', 'c': '2018-03 13:00'}})
+print(df)
+df = df.sort_values(by='시간')
+print(df)
